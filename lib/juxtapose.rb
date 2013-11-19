fe014700d32848447f1b6a7f681960388c51c653
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2013-11-19 09:52:19 -0500

    Remove debugging statement.

--- lib/juxtapose.rb
+++ lib/juxtapose.rb
@@ -6,7 +6,6 @@ require "juxtapose/version"
 
 Motion::Project::App.setup do |app|
   Dir.glob(File.join(File.dirname(__FILE__), 'juxtapose/**/*.rb')).reverse.each do |file|
-    puts "file: #{file}"
     app.files.unshift(file)
   end
 end
